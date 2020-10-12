087ce35ebb897616063a5079b33b884d928a34ce
JoëlPierreDupont <36246708+joelpierredupont@users.noreply.github.com>
2020-10-12 16:03:56 +0200

    Update Test7.py

--- Test7.py
+++ Test7.py
@@ -1,11 +1,12 @@
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe')  # Optional argument, if not specified will search path.
+# Test de chromedriver
+driver = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe')
 driver.get('http://www.google.com/');
-time.sleep(5) # Let the user actually see something!
+time.sleep(5) 
 search_box = driver.find_element_by_name('q')
 search_box.send_keys('ChromeDriver')
 search_box.submit()
-time.sleep(5) # Let the user actually see something!
+time.sleep(5) 
 driver.quit()
